35c93e59b254351941e645dfd792aae2d2d84414
Palak Mathur <palakmathur@gmail.com>
2026-09-17 22:44:33 -0500

    Add Vertico and separate configuration paths from Emacs state

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
-(org-babel-load-file (expand-file-name "systemhalted.org" user-emacs-directory))
+(defvar systemhalted/config-directory
+  (file-name-directory (or load-file-name buffer-file-name)))
+
+(org-babel-load-file (expand-file-name "systemhalted.org" systemhalted/config-directory))
--- test/run-config-tests.sh
+++ test/run-config-tests.sh
@@ -9,4 +9,4 @@ cp init.el systemhalted.org "$test_root"
 
 cd "$test_root"
 
-emacs --batch -Q --init-directory "$test_root" -l init.el
+emacs --batch -Q --init-directory "$test_root" -l init.el --eval '(unless (bound-and-true-p vertico-mode) (error "Vertico was not enabled"))'
