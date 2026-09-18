0c9077e9ab965401a2f81ea68f366a3580a056e9
Palak Mathur <palakmathur@gmail.com>
2026-09-17 23:10:59 -0500

    Add Orderless completion and matching smoke test

--- test/run-config-tests.sh
+++ test/run-config-tests.sh
@@ -9,4 +9,4 @@ cp init.el systemhalted.org "$test_root"
 
 cd "$test_root"
 
-emacs --batch -Q --init-directory "$test_root" -l init.el --eval '(unless (bound-and-true-p vertico-mode) (error "Vertico was not enabled"))'
+emacs --batch -Q --init-directory "$test_root" -l init.el --eval '(unless (bound-and-true-p vertico-mode) (error "Vertico was not enabled"))' --eval '(unless (equal (car (completion-all-completions "function describe" (quote ("describe-function" "find-file")) nil (length "function describe"))) "describe-function") (error "Orderless did not work as expected"))'
