81b1abaa6b8dc4dafac1e2c81817130064da5779
Palak Mathur <palakmathur@gmail.com>
2026-09-19 17:56:53 -0500

    Add ibuffer, config reload, markdown, and wordwise

--- init.el
+++ init.el
@@ -2,5 +2,7 @@
 
 (defvar systemhalted/config-directory
   (file-name-directory (or load-file-name buffer-file-name)))
+(defvar systemhalted/config-file
+  (expand-file-name "systemhalted.org" systemhalted/config-directory))
 
-(org-babel-load-file (expand-file-name "systemhalted.org" systemhalted/config-directory))
+(org-babel-load-file systemhalted/config-file)
